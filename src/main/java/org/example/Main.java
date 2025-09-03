abefc614791ed26d7334b53a03a42a3c9c04b6ef
jwan <jwanfaiisal@outlook.com>
2025-09-04 01:35:38 +0300

    inital

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+@Test
+    public static void main(String[] args) {
+//        WebDriver driver = new SafariDriver();
+//          driver.get("https://www.linkedin.com/hp");
+//,        driver.navigate().to("https://www.linkedin.com");
+
+//      int age = 11;
+//      Assertions.assertFalse(age <18);
+//
+
+    }}
+
